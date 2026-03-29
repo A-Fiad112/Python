40fb161b8f7710fc5b369f8e6dd9ac4a58480aee
Ahmed Fiad <fiadahmed112@gmail.com>
2026-03-29 18:00:56 +0200

    Update comprehensions.py

--- comprehensions.py
+++ comprehensions.py
@@ -13,6 +13,8 @@ print [n for n in nums]
 # for n in nums:
 #   my_list.append(n*n)
 # print my_list
+nums = [1,2,3,4,5,6,7,8,9,10]
+print(n*n for n in nums)
 
 # Using a map + lambda
 # my_list = map(lambda n: n*n, nums)
