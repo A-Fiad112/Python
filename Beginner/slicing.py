fb6ba95c92953251ff9187c25ee151f95b5098aa
Ahmed Fiad <fiadahmed112@gmail.com>
2026-03-29 20:38:57 +0200

    Update slicing.py

--- Beginner/slicing.py
+++ Beginner/slicing.py
@@ -6,7 +6,9 @@ my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # list[start:end:step]
 
 # print my_list[::-1]
-
+# print my_list[2:-1:2]
+# print my_list[-1:2:-1] --> [] 
+# print my_list[-2:1:-1]
 
 sample_url = 'http://coreyms.com'
 print sample_url
