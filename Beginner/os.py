2c1471aec867b1f5782b0ecea173d00161185cc8
Ahmed Fiad <fiadahmed112@gmail.com>
2026-03-31 18:03:56 +0200

    Update os.py

--- Beginner/os.py
+++ Beginner/os.py
@@ -1,5 +1,6 @@
 import os
 print(dir(os))
+print(dir(os.path))
 os.chdir() -> change directory
 print(os.getcwd()) -> get current working directory
 print(os.listdir()) -> folders and fiels on desktop
@@ -10,3 +11,9 @@ os.removedirs('os-demo-3/a') -> delete a folder and inner folder
 os.rename('old','new') -> with it's extension
 os.stat('demo.txt') -> this get the information of the file
 os.walk() -> very useful if u want let say know the folder and inner of this folder and files inner of these folder 
+os.path.basename('\temp\test.txt') <- any directory and this is not suppose to be real directory
+os.path.exists('\temp\test.txt') <- return true or false 
+os.path.isdir('\temp\test.txt') <- return true or false 
+os.path.isfile('\temp\test.txt') <- return true or false 
+os.path.split('\temp\test.txt') <- return true or false 
+os.path.splitext('\temp\test.txt') <- return true or false 
