e3ee1a63b0a6188ffd0f2f4491d0462df1c371a9
Ahmed Fiad <fiadahmed112@gmail.com>
2026-04-11 16:57:10 +0200

    Update formatting.py

--- Beginner/formatting.py
+++ Beginner/formatting.py
@@ -1,6 +1,10 @@
 
 person = {'name': 'Jenn', 'age': 23}
 
+# sentence = f"My name is {person['name']}  and I am  {str(person.get('age'))}  years old."
+# print(sentence)
+
+
 # sentence = 'My name is ' + person['name'] + ' and I am ' + str(person['age']) + ' years old.'
 # print(sentence)
 
@@ -20,8 +24,8 @@ person = {'name': 'Jenn', 'age': 23}
 # print(sentence)
 
 
-sentence = 'My name is {0} and I am {1} years old.'.format(person['name'], person['age'])
-print(sentence)
+# sentence = 'My name is {0} and I am {1} years old.'.format(person['name'], person['age'])
+# print(sentence)
 
 
 class Person():
@@ -30,48 +34,54 @@ class Person():
         self.name = name
         self.age = age
 
+
 p1 = Person('Jack', '33')
 
-sentence = 'My name is {0.name} and I am {0.age} years old.'.format(p1)
-print(sentence)
+# sentence = f'My name is {p1.name} and I am {p1.age} years old.'
+# print(sentence)
+# sentence = 'My name is {0.name} and I am {0.age} years old.'.format(p1)
+# print(sentence)
 
 # sentence = 'My name is {name} and I am {age} years old.'.format(name='Jenn', age='30')
 # print(sentence)
 
 # sentence = 'My name is {name} and I am {age} years old.'.format(**person)
 # print(sentence)
-
+#
 # for i in range(1, 11):
-#     sentence = 'The value is {}'.format(i)
-#     print(sentence)
-
-
-# pi = 3.14159265
-
-# sentence = 'Pi is equal to {}'.format(pi)
+# sentence = 'The value is {}'.format(i)
+# sentence = f'The value is {i:02}'
 
 # print(sentence)
 
 
-sentence = '1 MB is equal to {} bytes'.format(1000**2)
+pi = 3.14159265
+
+# sentence = 'Pi is equal to {}'.format(pi)
+sentence = f'Pi is equal to {pi:.2f}'
 
 print(sentence)
 
 
-import datetime
+# sentence = '1 MB is equal to {} bytes'.format(1000**2)
 
-my_date = datetime.datetime(2016, 9, 24, 12, 30, 45)
+# print(sentence)
+
+
+# import datetime
+
+# my_date = datetime.datetime(2016, 9, 24, 12, 30, 45)
 
 # print(my_date)
 
 # March 01, 2016
 
-sentence = '{:%B %d, %Y}'.format(my_date)
+# sentence = '{:%B %d, %Y}'.format(my_date)
 
-print(sentence)
-
+# print(sentence)
+#
 # March 01, 2016 fell on a Tuesday and was the 061 day of the year.
 
-sentence = '{:%B %d, %Y} fell on a {} and was the {} day of the year'.format(my_date)
+# /sentence = '{:%B %d, %Y} fell on a {} and was the {} day of the year'.format(my_date)
 
-print(sentence)
+# print(sentence)
